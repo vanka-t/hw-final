8efee14a961903154f2c265f12b6cfc07216585a
vanka-t <vt2068@nyu.edu>
2021-05-03 12:11:03 -0400

    player jump

--- sketch.js
+++ sketch.js
@@ -1,7 +1,59 @@
+var player;
+var playerIcon;
+
+function preload(){
+playerIcon = loadImage("images/smiley.png");
+}
+
 function setup() {
   createCanvas(400, 400);
+player = new PlayerSettings();
+playerIcon.resize(40,40);
+
+}
+
+function keyPressed(){
+  if(keyCode === UP_ARROW){
+    player.jump();
+  }
+  
 }
 
 function draw() {
   background(220);
+  player.show();
+  player.move();
+  fill(0);
+  //push();
+
+image(playerIcon, mouseX,mouseY);
+// translate(mouseX,mouseY);
+// pop();
+}
+
+
+class PlayerSettings{
+  constructor(){
+    this.r = 50;
+    this.x = 50;
+    this.y = height-this.r;
+    this.vy = 0;
+    this.gravity = 0.7;
+  }
+
+  jump(){
+    this.vy = -10;
+
+  }
+
+  move(){
+    this.y += this.vy;
+    this.vy += this.gravity;
+    this.y = constrain(this.y, 0, height - this.r);
+
+  }
+
+  show(){
+    rect(this.x,this.y,this.r,this.r);
+  }
 }
