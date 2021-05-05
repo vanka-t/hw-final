8fd8d478c73b7e7c6f3a60330c73cc2f54f36231
vanka-t <vt2068@nyu.edu>
2021-05-04 23:31:35 -0400

    ooga

--- sketch.js
+++ sketch.js
@@ -10,11 +10,18 @@ var bar;
 var playerIcon;
 var barIcon;
 var enemyIcon;
+var gameOverIcon;
+var tryAgainIcon;
+
+
+
 
 function preload(){
 playerIcon = loadImage("images/smiley.png");
 barIcon = loadImage("images/brickwall.jpg");
 enemyIcon = loadImage("images/fire.png");
+gameOverIcon = loadImage("images/game-over.jpg")
+tryAgainIcon = loadImage("images/try-again.jpg")
 
 }
 
@@ -28,11 +35,19 @@ enemy1 = new Enemy();
 playerIcon.resize(40,40);
 barIcon.resize(150,50);
 enemyIcon.resize(70,70);
+gameOverIcon.resize(400,270);
+tryAgainIcon.resize(200,120);
 console.log("ome");
+tryAgainIcon.mousePressed(yay);
+
 
 //bars.push(new FloatingBars());
 
 }
+
+function yay(){
+  console.log(yay);
+} 
 
 function keyPressed(){
   if(keyCode === UP_ARROW){
@@ -40,7 +55,17 @@ function keyPressed(){
   }
 }
 
+// function mousePressed(){
+//   if(tryAgainIcon){
+//     console.log("yay");
+//   }
+// }
+
 function draw() {
+  var xPos = width/2; //controlling position for all text icons
+  var yPos = height/2;
+
+
    if(random(1) <0.01){ //bars showing up irregularly -->decimal value = probability of bar showing up
     enemies.push(new Enemy());
   }
@@ -48,7 +73,7 @@ function draw() {
   background(220);
   player.show();
   player.move();
-  
+
   for (let e of enemies){
      e.move();
      e.show();
@@ -56,17 +81,20 @@ function draw() {
       if (player.hits(e)){
         console.log("u suck");
         noLoop();
+        imageMode(CENTER);
+        image(gameOverIcon,xPos,yPos);
+        image(tryAgainIcon,xPos+50,yPos+100);
       }
   }
+
+
+
   // bar1.make();
 
   
    bar1.show();
    bar1.move();
   //bar = createSprite(50,50,100,150);
-       
-
-     
    //  player.collide(bar1);
   //  }
 
